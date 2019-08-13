323f4a1e10bfc0bea3538a607dfd00e733087adc
William Wong <wwong1987@gmail.com>
2019-08-12 22:52:22 -0700

    working on push() drills

--- drills.js
+++ drills.js
@@ -78,3 +78,32 @@ class Array {
 
 Array.SIZE_RATIO = 3; // if the length is greater than the capacity then you resize according to the SIZE_RATIO. In this case, each time you go over the capacity, you triple the size of memory which is allocated.
 // tradeoff = wasting some memory when the capacity is greater than the length. 
+
+// =========== Explore the push() method ================
+// Using the Array class you just created above, add an item to the array. Use the following function:
+
+function main() {
+    Array.SIZE_RATIO = 3;
+
+    // Create an instance of the Array class
+    let arr = new Array();
+
+    // Add an item to the array
+    arr.push(3);
+
+    console.log('line 94', arr); // Array { length: 1, _capacity: 1, ptr: 0 }
+}
+
+main();
+
+// TODO: What is the length, capacity and memory address of your array?
+
+// TODO: Add the following in the main function and then print the array:
+
+    // arr.push(5);
+    // arr.push(15);
+    // arr.push(19);
+    // arr.push(45);
+    // arr.push(10);
+
+// TODO: What is the length, capacity and memory address of your array? Explain the result of your program after adding the new lines of code.
