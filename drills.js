1cfd8f587b31e88888e8290f63e12a5b9dab4c0c
William Wong <wwong1987@gmail.com>
2019-08-12 18:07:54 -0700

    initial commit with array definition and memory file

---
+++ drills.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* 
+Arrays = an ordered sequence of data (and potentially empty spaces). Data is stored in contiguous memory (all of the array data is stored in order at subsequent memory addresses)
+
+
+
+
+*/
